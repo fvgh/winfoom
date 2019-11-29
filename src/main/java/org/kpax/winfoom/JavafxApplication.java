ce35735666bf2375046b5cc96f8741662534bda2
ecovaci <eugen.covaci.q@gmail.com>
2019-11-29 14:15:26 +0200

    Useless close the splash screen removed.

--- src/main/java/org/kpax/winfoom/JavafxApplication.java
+++ src/main/java/org/kpax/winfoom/JavafxApplication.java
@@ -16,7 +16,6 @@ import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.kpax.winfoom.config.UserConfig;
 import org.kpax.winfoom.proxy.LocalProxyServer;
 import org.kpax.winfoom.util.GuiUtils;
-import org.kpax.winfoom.util.LocalIOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -28,10 +27,8 @@ import org.springframework.core.io.Resource;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.File;
 import java.nio.file.Paths;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 public class JavafxApplication extends Application {
 
@@ -50,11 +47,13 @@ public class JavafxApplication extends Application {
                         .propertiesBuilder(Paths.get(System.getProperty("user.dir"), "config",
                                 "user.properties").toFile()));
             };
-
             SpringApplication springApplication = new SpringApplication(FoomApplication.class);
             springApplication.addInitializers(initializer);
             this.applicationContext = springApplication.run(getParameters().getRaw().toArray(new String[0]));
         } finally {
+            // Initializing Spring context is
+            // the only time consuming task,
+            // so we close the splash screen.
             GuiUtils.closeAllAwtWindows();
         }
 
@@ -67,52 +66,47 @@ public class JavafxApplication extends Application {
         // Preventing exit on close
         Platform.setImplicitExit(false);
 
-        Scene scene;
-        try {
-            Resource fxml = this.applicationContext.getResource("classpath:/view/main.fxml");
-            FXMLLoader fxmlLoader = new FXMLLoader(fxml.getURL());
-            fxmlLoader.setControllerFactory(this.applicationContext::getBean);
-            Parent root = fxmlLoader.load();
+        Resource fxml = this.applicationContext.getResource("classpath:/view/main.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxml.getURL());
+        fxmlLoader.setControllerFactory(this.applicationContext::getBean);
+        Parent root = fxmlLoader.load();
 
-            scene = new Scene(root);
-            this.primaryStage.setScene(scene);
-            this.primaryStage.setTitle("WinFoom");
-            this.primaryStage.getIcons().add(
-                    new javafx.scene.image.Image(Paths.get("./config/img/icon.png").toUri().toURL().toExternalForm()));
+        Scene scene = new Scene(root);
+        this.primaryStage.setScene(scene);
+        this.primaryStage.setTitle("WinFoom");
+        this.primaryStage.getIcons().add(
+                new javafx.scene.image.Image(Paths.get("./config/img/icon.png").toUri().toURL().toExternalForm()));
 
-            if (SystemTray.isSupported()) {
-                Image iconImage = Toolkit.getDefaultToolkit().getImage("config/img/icon.png");
-                final TrayIcon trayIcon = new TrayIcon(iconImage, "Basic Proxy Facade");
-                trayIcon.setImageAutoSize(true);
-                trayIcon.addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        Platform.runLater(() -> {
-                            primaryStage.setIconified(false);
-                            primaryStage.show();
-                        });
-                    }
-                });
-                final SystemTray tray = SystemTray.getSystemTray();
-                this.primaryStage.iconifiedProperty().addListener((observableValue, oldVal, newVal) -> {
-                    if (newVal != null) {
-                        if (newVal) {
-                            try {
-                                tray.add(trayIcon);
-                                Platform.runLater(primaryStage::hide);
-                            } catch (AWTException ex) {
-                                logger.error("Cannot add icon to tray", ex);
-                            }
-                        } else {
-                            tray.remove(trayIcon);
+        if (SystemTray.isSupported()) {
+            Image iconImage = Toolkit.getDefaultToolkit().getImage("config/img/icon.png");
+            final TrayIcon trayIcon = new TrayIcon(iconImage, "Basic Proxy Facade");
+            trayIcon.setImageAutoSize(true);
+            trayIcon.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    Platform.runLater(() -> {
+                        primaryStage.setIconified(false);
+                        primaryStage.show();
+                    });
+                }
+            });
+            final SystemTray tray = SystemTray.getSystemTray();
+            this.primaryStage.iconifiedProperty().addListener((observableValue, oldVal, newVal) -> {
+                if (newVal != null) {
+                    if (newVal) {
+                        try {
+                            tray.add(trayIcon);
+                            Platform.runLater(primaryStage::hide);
+                        } catch (AWTException ex) {
+                            logger.error("Cannot add icon to tray", ex);
                         }
+                    } else {
+                        tray.remove(trayIcon);
                     }
-                });
-            } else {
-                logger.warn("Icon tray not supported!");
-            }
-        } finally {
-            GuiUtils.closeAllAwtWindows();
+                }
+            });
+        } else {
+            logger.warn("Icon tray not supported!");
         }
 
         this.primaryStage.show();
