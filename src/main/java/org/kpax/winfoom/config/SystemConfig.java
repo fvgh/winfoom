56c30841812f2d28a789d76a78eff0e4ece5ea29
ecovaci <eugen.covaci.q@gmail.com>
2020-02-21 07:58:55 +0200

    Useless debugging removed.

--- src/main/java/org/kpax/winfoom/config/SystemConfig.java
+++ src/main/java/org/kpax/winfoom/config/SystemConfig.java
@@ -67,8 +67,7 @@ public class SystemConfig {
                     .get(Attributes.Name.IMPLEMENTATION_VERSION).toString();
         } catch (Exception e) {
             releaseVersion = "Unknown";
-            logger.warn("Cannot get application version from MANIFEST file. Use debug mode to investigate!");
-            logger.debug("Error on getting application version", e);
+            logger.warn("Error on getting application version from MANIFEST file", e);
         }
     }
 
