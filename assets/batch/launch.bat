484086f4a96e6359ee0e8e04cfa99327c2aba068
ecovaci <eugen.covaci.q@gmail.com>
2020-04-11 12:21:47 +0300

    Add launcher to git repo

---
+++ assets/batch/launch.bat
@@ -0,0 +1,31 @@
+:: Launcher for Winfoom - Basic Proxy Facade
+
+echo off
+
+setlocal EnableDelayedExpansion
+
+set ARGS=-server
+
+FOR %%a IN (%*) DO (
+
+    IF NOT "%%a"=="--debug" IF NOT "%%a"=="--systemjre" (
+		echo Unknow parameter: %%a
+		exit 1;
+	)
+
+	IF "%%a"=="--debug" (
+		SET ARGS=%ARGS% -Dlogging.level.root=DEBUG
+	)
+
+	IF "%%a"=="--systemjre" (
+		SET JAVA_EXE=javaw
+	)
+
+)
+
+IF NOT DEFINED JAVA_EXE set JAVA_EXE=jdk/bin/javaw
+
+echo JAVA_EXE=%JAVA_EXE%
+echo ARGS=%ARGS%
+
+start %JAVA_EXE% %ARGS% -cp . -jar winfoom.jar
